2bad06bf1e5f781c7603e83c1a77fea14e3a2ba1
jhuguetn <jhuguetn@gmail.com>
2016-09-12 14:35:37 +0200

    Replacing tabs for whitespaces

--- freesurfer/resource/fs_reconAll.sh
+++ freesurfer/resource/fs_reconAll.sh
@@ -13,20 +13,20 @@ set -e
 if [ $# != 3 ]
   then
         #echo "No valid arguments supplied"
-		echo "FreeSurfer recon-all launcher - v1.1"	
+        echo "FreeSurfer recon-all launcher - v1.1"    
         exit 1
   else
         IN_DIR=$1
         SUBJECT_NAME=$2
-		OUT_DIR=$3
+        OUT_DIR=$3
         DCM_FILE=$(find $IN_DIR -type f -iname '*.dcm' | sort -n | head -1)
-		
-		#module load freesurfer/5.3.0
-		#export FREESURFER_HOME=/opt/amc/freesurfer-5.3.0
-		#export NO_FSFAST=1
-		#source /opt/amc/freesurfer-5.3.0/FreeSurferEnv.sh
-        echo nice -n 19 recon-all -i $DCM_FILE -s $SUBJECT_NAME -sd $OUT_DIR -autorecon-all #-autorecon1		
-		nice -n 19 recon-all -i $DCM_FILE -s $SUBJECT_NAME -sd $OUT_DIR -autorecon-all #-autorecon1		
+        
+        #module load freesurfer/5.3.0
+        #export FREESURFER_HOME=/opt/amc/freesurfer-5.3.0
+        #export NO_FSFAST=1
+        #source /opt/amc/freesurfer-5.3.0/FreeSurferEnv.sh
+        echo nice -n 19 recon-all -i $DCM_FILE -s $SUBJECT_NAME -sd $OUT_DIR -autorecon-all #-autorecon1        
+        nice -n 19 recon-all -i $DCM_FILE -s $SUBJECT_NAME -sd $OUT_DIR -autorecon-all #-autorecon1        
 fi
 
 exit 0
