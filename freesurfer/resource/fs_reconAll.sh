4fb33f94a3b2318b32b81953b6165368b7c461a3
jhuguetn <jhuguetn@gmail.com>
2016-09-16 12:34:45 +0200

    Script can use as input data either NIFTI (preferred) or DICOM format structural scan data

--- freesurfer/resource/fs_reconAll.sh
+++ freesurfer/resource/fs_reconAll.sh
@@ -13,27 +13,31 @@ set -e
 if [ $# != 3 ]
   then
         #echo "No valid arguments supplied"
-        echo "FreeSurfer recon-all launcher - v1.1"    
+        echo "FreeSurfer recon-all launcher - v1.2"    
         exit 1
   else
         IN_DIR=$1
         SUBJECT_NAME=$2
         OUT_DIR=$3
-        DCM_FILE=$(find $IN_DIR -type f -iname '*.dcm' | sort -n | head -1)
         
-        #module load freesurfer/5.3.0
-        #export FREESURFER_HOME=/opt/amc/freesurfer-5.3.0
-        #export NO_FSFAST=1
-        #source /opt/amc/freesurfer-5.3.0/FreeSurferEnv.sh
+        #try using NIFTI scan files if available
+        IN_FILE=$(find $IN_DIR -type f -iname '*.nii' | sort -n | head -1)
+        
+        #use DICOM-formatted data otherwise
+        if [[ -z $IN_FILE ]]
+        then 
+            IN_FILE=$(find $IN_DIR -type f -iname '*.dcm' | sort -n | head -1)
+        fi
         
         # Jordi Huguet - 20160912 - workaround to fix an issue where FreeSurfer assumes structural scan is DWI and crashes
+        # log message:
         #   ERROR: GetDICOMInfo(): dcmGetDWIParams() 7
         #   IsDWI = 1, IsPhilipsDWI = 1
         #   This is a philips DWI, so ignorning the last frame, nframes = 0
         export FS_LOAD_DWI=0
         
-        echo nice -n 19 recon-all -i $DCM_FILE -s $SUBJECT_NAME -sd $OUT_DIR -autorecon-all #-autorecon1        
-        nice -n 19 recon-all -i $DCM_FILE -s $SUBJECT_NAME -sd $OUT_DIR -autorecon-all #-autorecon1        
+        #lower down process priority by max. 19 not to hog resources
+        nice -n 19 recon-all -i $IN_FILE -s $SUBJECT_NAME -sd $OUT_DIR -autorecon-all #-autorecon1        
 fi
 
 exit 0
