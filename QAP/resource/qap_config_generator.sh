ab4f95b5288db6a1939cc10cf8f69e427f84dcb4
jhuguetn <jhuguetn@gmail.com>
2017-01-11 12:24:27 +0100

    New feature for DEBUG MODE execution of QAP pipelines.

--- QAP/resource/qap_config_generator.sh
+++ QAP/resource/qap_config_generator.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 ##
-## title:             qap_config_generator.sh
+## title:        qap_config_generator.sh
 ## description:  Create a QAP specific configuration file (YAML format) for running the QAP package.
-##                      Script simply writes baseline configuration options to file line-by-line and additionally enters
-##                      argument-based output and working directories.
+##               Script simply writes baseline configuration options to file line-by-line and additionally enters
+##               argument-based output and working directories.
 ##
-## inputs:          (1) output directory
-##                      (2) working directory
-##                      (3) generated configuration file name
-##                      (4) scans type, either {anat,func}
-## author:          Jordi Huguet (AMC)
-## date:             20160922
-## version:         0.1
-## usage:           bash qap_config_generator.sh [output_qap_directory] [working_qap_directory] [config_file_path] [scans_type]
+## inputs:       (1) output directory
+##               (2) working directory
+##               (3) generated configuration file name
+##               (4) scans type, either {anat,func}
+##               (5) debug mode flag, either {Y,N}
+## author:       Jordi Huguet (AMC)
+## date:         20170111
+## version:      0.2
+## usage:        bash qap_config_generator.sh [output_qap_directory] [working_qap_directory] [config_file_path] [scans_type] [DEBUG_MODE_FLAG]
 
 
-if [ $# != 4 ]
+if [ $# != 5 ]
   then
         echo "generate QAP configuration file (QAP package) - v0.1"
         exit 1
@@ -25,6 +26,7 @@ if [ $# != 4 ]
         WORKING_DIRECTORY=$2
         CONFIG_FILENAME=$3
         SCANS_TYPE=$4
+        DEBUG_MODE_FLAG=$5
 
         echo "num_cores_per_subject: 1" >> $CONFIG_FILENAME
         echo "num_subjects_at_once: 1" >> $CONFIG_FILENAME
@@ -32,9 +34,13 @@ if [ $# != 4 ]
         echo "working_directory: "$WORKING_DIRECTORY >> $CONFIG_FILENAME
         # Jordi Huguet (20160510) - "IOError: Duplicate node name datasink_qap_mosaic found"
         # Error fix not yet available, workaround: choose between having 'write_reports' or 'write_all_outputs' set to True.
-        #write_all_outputs: True
-        echo "write_all_outputs: False" >> $CONFIG_FILENAME
-        echo "write_report: True" >> $CONFIG_FILENAME
+        if [ $DEBUG_MODE_FLAG == "N" ]; then
+            echo "write_all_outputs: False" >> $CONFIG_FILENAME
+            echo "write_report: True" >> $CONFIG_FILENAME
+        elif [ $DEBUG_MODE_FLAG == "Y" ]; then
+            echo "write_all_outputs: True" >> $CONFIG_FILENAME
+            echo "write_report: False" >> $CONFIG_FILENAME
+        fi
         echo "write_graph: True" >> $CONFIG_FILENAME
         if [ $SCANS_TYPE == "func" ]; then
 
