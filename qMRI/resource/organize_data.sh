adf2b1c3ae5d547612208ef2ed3523380fcac0d7
jhuguetn <jhuguetn@gmail.com>
2017-12-05 15:49:54 +0100

    Pipeline refactored to get/use B1 map output product from convertB1 pipeline

--- qMRI/resource/organize_data.sh
+++ qMRI/resource/organize_data.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 ##
 ## title:            organize_data.sh
-## description:      Search for XNAT-downloaded MRI scan files, clean up unnecessary stuff
+## description:      Organize XNAT-downloaded MRI scan files, clean up unnecessary stuff
 ##                   and reshape the file/directory structure for simplicity.
 ## inputs:           (1) Locally accessible copy of XNAT retrieved imaging data 
 ## author:           Jordi Huguet (AMC)
-## date:             20170717
-## version:          0.3.2
+## date:             20171205
+## version:          1.1.1
 ## usage:            bash organize_data.sh [input directory]
 
 
-if [ $# != 1 ]
+if [ $# != 2 ]
   then
-        echo "organize XNAT downloaded scan data for qMRI pipeline analysis - v0.3.1"
+        echo "organize XNAT downloaded scan data for qMRI pipeline analysis - v1.1.1"
         exit 1        
   else
         INPUT_DIRECTORY=$1
+        B1_DIRECTORY=$2
         
         SCANS_DIR=$(find $INPUT_DIRECTORY -type d -iname "SCANS")
         if [ ! -d $SCANS_DIR ]; then
@@ -27,34 +28,33 @@ if [ $# != 1 ]
         mkdir -v -p $PARENT_SCANS_DIR/SORTED/B1
         mkdir -v -p $PARENT_SCANS_DIR/SORTED/T1
         
-        for SCAN_DIR in $(find $SCANS_DIR -mindepth 1 -maxdepth 1 -type d -iname '*B1'); do
-            
-            # TO-DO: assert that there are 4 files (2 Magn + 2 Phase) or 6 files (3 Magn + 3 Phase)
-            
-            B1_NIFTIs=$(find $SCAN_DIR -type f -iname "*.nii" -o -iname "*.nii.gz")
-            if [[ -z $B1_NIFTIs ]]; then
-                echo "[error] No valid B1 datasets found: "$SCAN_DIR
-                exit 1
-            fi
-            
-            for B1_NII_FILE in $B1_NIFTIs; do
-            
-                # TO-DO: rename or refactor the name scheme used to be homogeneous (?)
-                # HINT --> use file size to distinguish between Phase and Magnitude files
-                
-                mv -v $B1_NII_FILE $PARENT_SCANS_DIR/SORTED/B1
-                NEW_FILE_PATH=$PARENT_SCANS_DIR/SORTED/B1/$(basename $B1_NII_FILE)
-                
-                #decompress (gzip) for harmonizing and/or compatibility with Matlab code?
-                if [[ $(basename $B1_NII_FILE) == *.nii.gz ]]
-                  then
-                    gzip -v -d $NEW_FILE_PATH
-                    NEW_FILE_PATH=${NEW_FILE_PATH::-3}
-                fi
-                                
-            done            
-        done
+        # B1 map data sorting    
+        #
+        B1_NIFTIs=$(find $B1_DIRECTORY -type f -iname "*.nii" -o -iname "*.nii.gz")
+        if [[ -z $B1_NIFTIs ]]; then
+            echo "[error] No valid B1 maps found: "$B1_DIRECTORY
+            exit 1
+        fi
         
+        for B1_NII_FILE in $B1_NIFTIs; do
+        
+            # TO-DO: rename or refactor the name scheme used to be homogeneous (?)
+            # HINT --> use file size to distinguish between Phase and Magnitude files
+            
+            mv -v $B1_NII_FILE $PARENT_SCANS_DIR/SORTED/B1
+            NEW_FILE_PATH=$PARENT_SCANS_DIR/SORTED/B1/$(basename $B1_NII_FILE)
+            
+            #decompress (gzip) for harmonizing and/or compatibility with Matlab code?
+            if [[ $(basename $B1_NII_FILE) == *.nii.gz ]]
+              then
+                gzip -v -d $NEW_FILE_PATH
+                NEW_FILE_PATH=${NEW_FILE_PATH::-3}
+            fi
+                            
+        done            
+        
+        # T1w scan data sorting
+        #
         for SCAN_DIR in $(find $SCANS_DIR -mindepth 1 -maxdepth 1 -type d -iname '*T1*M' -o -iname '*T1*P' ); do
             
             # TO-DO: assert that there are either 5 (Magn/Phase only) or 10 files (5 Magn + 5 Phase)
